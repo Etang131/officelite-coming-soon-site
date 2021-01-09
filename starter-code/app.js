8eebeb5e64d1633a148e4c702d9e8cd362545d38
Erik Tangvik <erikjt@outlook.com>
2021-01-09 09:35:09 -0600

    added notes on how to start javascript

---
+++ starter-code/app.js
@@ -0,0 +1,11 @@
+//Get Current Date
+//store future date to be 30 days from current date to make count dynamic
+//Set html to be 30 days from current date
+//countdown from date
+//stop counting down when you reach the date
+
+// Time calculations for days, hours, minutes and seconds
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
