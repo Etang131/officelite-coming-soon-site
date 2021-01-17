23f90ef2773b0edad35057a25ef9e138204c0d54
Erik Tangvik <erikjt@outlook.com>
2021-01-17 09:53:05 -0600

    Countdown works but need to add the day it's count too

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,27 +1,35 @@
-//Get Current Date
-var currentDate = new Date();
+// Set the date we're counting down to
+//The Future date needs to be outside of the set interval so it doesn't keep resetting
+//To set it to a certain day use this
+//var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+var futureDate = new Date(
+  Date.parse(new Date()) + 30 * 24 * 60 * 60 * 1000
+).getTime();
 
-//Set the future date
-var futureDate = new Date(Date.parse(new Date()) + 30 * 24 * 60 * 60 * 1000);
-
+// Update the count down every 1 second
 var x = setInterval(function () {
-  // Find the distance between now and the future date
-  var distance = futureDate - currentDate;
+  // Get today's date and time
+  var now = new Date().getTime();
 
-  // // Time calculations for days, hours, minutes and seconds
+  // Find the distance between now and the future date
+  var distance = futureDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  //Display the result
+  // Output the result in an element with id="demo"
   document.getElementById("daysUnit").innerText = days;
   document.getElementById("hoursUnit").innerText = hours;
   document.getElementById("minsUnit").innerText = minutes;
   document.getElementById("secoundsUnit").innerText = seconds;
 
-  // let countDownDayDisplay = document.getElementById("timeTextBold");
+  // Countdown Date text above counter
+  //document.getElementById("timeTextBold").innerText = futureDate.Date("");
 
+  // If the count down is over, write some text
   if (distance < 0) {
     clearInterval(x);
   }
